14253a7bf69b75a6e1ec2a546164300380c27f4e
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-10 07:45:15 +0900

    guessing_game: compare with secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,8 @@ extern crate rand; // 외부에 의존하는 크레이트, `cargo doc --open`으
 
 // 사용자의 입력을 받고 결과값을 표시하기 위한 라이브러리
 use std::io;
+// Result같은 열거형 (Less, Greater, Equal)
+use std::cmp::Ordering;
 // 정수 생성기가 구현한 메소드를 정의한 `trait`
 use rand::Rng;
 
@@ -25,4 +27,12 @@ fn main() {
         .expect("Failed to read line");  // Err일 때 실행하는 메소드. 프로그램 작동을 멈추고 인자를 출력
 
     println!("You guessed: {}", guess); // placeholder 사용
+
+    // 작동하지 않는다. 형태가 일치하지 않음
+    // secret_number: u32, guess: String
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too small!"),
+        Ordering::Equal => println!("Too small!"),
+    }
 }
