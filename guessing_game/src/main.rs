d01d7b57ca79dcce2eb97274cd861020bf3837ef
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-10 07:51:52 +0900

    guessing_game: use loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -14,29 +14,31 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // 러스트에서 변수는 기본적으로 불변,
-    // 그래서 mut(mutable) 키워드를 사용해서 가변함수를 만든다.
-    let mut guess = String::new();
-    // :: 는 new가 String타입의 연관함수임을 나타낸다.
-    // 연관함수는 하나의 타입을 위한 함수.
-    // String 인스턴스가 아니라 String 타입을 위한 함수 (static method)
+        // 러스트에서 변수는 기본적으로 불변,
+        // 그래서 mut(mutable) 키워드를 사용해서 가변함수를 만든다.
+        let mut guess = String::new();
+        // :: 는 new가 String타입의 연관함수임을 나타낸다.
+        // 연관함수는 하나의 타입을 위한 함수.
+        // String 인스턴스가 아니라 String 타입을 위한 함수 (static method)
 
-    io::stdin().read_line(&mut guess)  // io::Result => Ok 또는 Err
-        .expect("Failed to read line");  // Err일 때 실행하는 메소드. 프로그램 작동을 멈추고 인자를 출력
+        io::stdin().read_line(&mut guess)  // io::Result => Ok 또는 Err
+            .expect("Failed to read line");  // Err일 때 실행하는 메소드. 프로그램 작동을 멈추고 인자를 출력
 
-    // parse를 통한 형 변환
-    // trim으로 \n 제거
-    // 타입 명시를 통해 정해준다.
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number");
+        // parse를 통한 형 변환
+        // trim으로 \n 제거
+        // 타입 명시를 통해 정해준다.
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number");
 
-    println!("You guessed: {}", guess); // placeholder 사용
+        println!("You guessed: {}", guess); // placeholder 사용
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => println!("You win!"),
+        }
     }
 }
