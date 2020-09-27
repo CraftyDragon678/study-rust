709e6cccf1fcf2df93b641b0f0288db983482330
CraftyDragon678 <craftydragon678@gmail.com>
2020-09-27 13:47:09 +0900

    mutable references

--- references/src/main.rs
+++ references/src/main.rs
@@ -4,8 +4,16 @@ fn main() {
     let len = calc_len(&s1);
 
     println!("{} {}", s1, len);
+
+    let mut s1 = s1;
+    mutation_str(&mut s1);
+    println!("{}", s1);
 }
 
 fn calc_len(s: &String) -> usize {
     s.len()
+}
+
+fn mutation_str(s: &mut String) {
+    s.push_str(", world");
 }
