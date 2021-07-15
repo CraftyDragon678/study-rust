b943d8f67b73c3aff64099c2012ef80aa20f5d60
CraftyDragon678 <craftydragon678@gmail.com>
2021-07-15 21:08:46 +0900

    scope

--- references/src/main.rs
+++ references/src/main.rs
@@ -8,6 +8,11 @@ fn main() {
     let mut s1 = s1;
     mutation_str(&mut s1); // 가변 참조자 생성
     println!("{}", s1);
+
+    let r1 = &mut s1;
+    // let r2 = &mut s1; 데이터 레이스: 가변 참조자를 같은 스코프에서 여러 개 만들 수 없다.
+    println!("r1: {}", r1);
+    // println!("r2: {}", r2);
 }
 
 fn calc_len(s: &String) -> usize {
